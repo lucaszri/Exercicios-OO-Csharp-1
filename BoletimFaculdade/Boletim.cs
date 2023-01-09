801a8875a77ede7f025eb6bb1a31160ef1d1ee9f
Lucas de Souza <lucaszsep@gmail.com>
2023-01-09 17:21:33 -0300

    Exercício 1, criar boletim.

---
+++ BoletimFaculdade/Boletim.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoletimFaculdade
+{
+    public class Boletim
+    {
+        //nome do aluno
+        public string Nome { get; set; }
+        
+        // tem que iniciar em 1964
+        public int Ano { get; set; }
+
+        // 1 ao 14
+        public int Semestre { get; set; }
+
+        public Dictionary<string, double> Materias = new Dictionary<string, double>();
+
+        public Boletim(string nome, int ano, int semestre)
+        {
+            this.Nome = nome;
+                if(ano < 1964 || ano > 2023)
+                {
+                    Console.WriteLine("Ano inválido! Ele tem que ser maior que 1964 e menor que o atual.");
+                } else
+                {
+                    this.Ano = ano;
+                }
+                if (semestre < 1 || semestre > 14)
+                {
+                    Console.WriteLine("Número do semestre inválido, ele deve ser entre 1 e 14.");
+                }
+                else
+                {
+                    this.Semestre = semestre;
+                }
+        }
+    }
+}
---
+++ BoletimFaculdade/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BoletimFaculdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boletim aluno = new Boletim("lucas", 1990, 5);
+            Console.WriteLine(aluno.Ano);
+
+            aluno.Materias.Add("Matematica" , 4.6);
+            aluno.Materias.Add("Português", 6.3);
+            aluno.Materias.Add("Ingês" , 7.1);
+            aluno.Materias.Add("Ed. Física" , 6.0);
+            aluno.Materias.Add("Quimica" , 9.6);
+
+            foreach (KeyValuePair<string, double> entry in aluno.Materias)
+            {
+                Console.WriteLine($"Na matéria {entry.Key}, sua nota foi: {entry.Value}");
+            }
+        }
+    }
+}
+    

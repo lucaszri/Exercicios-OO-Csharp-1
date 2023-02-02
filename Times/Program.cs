a12014f07db7493ceaa851d0b6f6cad71f154fec
Lucas de Souza <lucaszsep@gmail.com>
2023-02-02 16:28:37 -0300

    Adicionado exercicio times

---
+++ Times/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Times
+{
+    internal class Program
+    {
+        /*Crie uma classe Time que tenha como propriedade o nome do país, 
+         * nome do esporte e a pontuação dele em um torneio. 
+         * Sua pontuação pode ser acrescida por um método pontua que recebe como parâmetro "W" para vitória (+3 pontos),
+         * "L" para derrota (+0 pontos) e "D" para empate (+1 ponto)*/
+        static void Main(string[] args)
+        {
+           Time time = new Time("Palmeiras" , "Futebol");
+
+            time.Pontuar(time);
+        }
+    }
+}
---
+++ Times/Time.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Times
+{
+    public class Time
+    {
+        public string Nome { get; set; }
+        public string Esporte { get; set; }
+        public int Pontos { get; set; }
+
+        public Time(string nome, string esporte)
+        {
+            Nome = nome;
+            Esporte = esporte;
+        }
+
+        public void Pontuar(Time time)
+        {
+            Console.WriteLine("Digite W para VITÓRIA");
+            Console.WriteLine("Digite D para EMPATE");
+            Console.WriteLine("Digite L para DERROTA");
+            string resultado = Console.ReadLine();
+
+            switch (resultado) 
+            {
+                case "W":
+                    time.Pontos += 3;
+                    Console.WriteLine("Você venceu, e somou mais 3 pontos");
+                    break;
+                case "D":
+                    time.Pontos += 1;
+                    Console.WriteLine("Você empatou, e somou mais 1 ponto");
+                    break;
+                case "L":
+                    Console.WriteLine("Você perdeu, e não somou pontos");
+                    break;
+                default: 
+                    Console.WriteLine("Opção inválida!");
+                    break;
+
+            }
+        }
+    }
+}
